4773fc36122696c83ffa730c7afe42b4064688c6
Dinh Truong Phan <dnhtruong46.cs.pyth@gmail.com>
2024-02-22 15:45:19 +0700

    end lab 2 and formatted

--- course2/lec2/cursor.sql
+++ course2/lec2/cursor.sql
@@ -1,145 +1,169 @@
-
 --1--
 DECLARE
-    CURSOR c_products IS
-        SELECT 
-            PRODUCT_CD,
-            NAME
-        FROM PRODUCT;
-    v_products c_products%ROWTYPE;
+    CURSOR C_PRODUCTS IS
+    SELECT
+        PRODUCT_CD,
+        NAME
+    FROM
+        PRODUCT;
+    V_PRODUCTS C_PRODUCTS%ROWTYPE;
 BEGIN
-    OPEN c_products;
-        LOOP
-            FETCH c_products INTO v_products;
-            EXIT WHEN c_products%notfound;
-            DBMS_OUTPUT.PUT_LINE('Product CD: ' || v_products.PRODUCT_CD);
-            DBMS_OUTPUT.PUT_LINE('Name         : ' || v_products.NAME);
-            DBMS_OUTPUT.PUT_LINE(' ');
-        END LOOP;
-    CLOSE c_products;
-END;
-
-
---2--
-DECLARE
-    CURSOR c_infor IS
-        SELECT
-            A.CUST_ID,
-            A.PRODUCT_CD,
-            P.NAME
-        FROM ACCOUNT A
-        JOIN PRODUCT P
-            ON A.PRODUCT_CD = P.PRODUCT_CD;
-    v_infor c_infor%ROWTYPE;
-BEGIN
-    OPEN c_infor;
-        LOOP
-            FETCH c_infor INTO v_infor;
-            EXIT WHEN c_infor%notfound;
-            DBMS_OUTPUT.PUT_LINE('Customer ID: ' || v_infor.CUST_ID || '   - Product CD: ' || v_infor.PRODUCT_CD || '   - Product Name: ' || v_infor.NAME);
-        END LOOP;
-    CLOSE c_infor;
-END;
-
-
---3--
-DECLARE
-    CURSOR c_cursor IS
-        SELECT
-            I.FIRST_NAME || ' ' || I.LAST_NAME AS NAME,
-            SUM(A.AVAIL_BALANCE) AS AVAIL_BALANCE
-        FROM CUSTOMER C
-        JOIN ACCOUNT A
-            ON C.CUST_ID = A.CUST_ID
-        JOIN INDIVIDUAL I
-            ON I.CUST_ID = C.CUST_ID
-        GROUP BY I.FIRST_NAME || ' ' || I.LAST_NAME;
-    
-    v_customer c_cursor%ROWTYPE;
-    v_segment VARCHAR2(20);
-BEGIN
-    OPEN c_cursor;
-        LOOP
-            FETCH c_cursor INTO v_customer;
-            IF v_customer.AVAIL_BALANCE <= 4000 
-                THEN v_segment := 'Low';
-            ELSIF v_customer.AVAIL_BALANCE > 4000 AND v_customer.AVAIL_BALANCE <= 7000
-                THEN v_segment := 'Medium';
-            ELSIF v_customer.AVAIL_BALANCE > 7000
-                THEN v_segment := 'High';
-            END IF;
-
-            EXIT WHEN c_cursor%notfound;
-
-            DBMS_OUTPUT.PUT_LINE('Customer: ' || v_customer.NAME);
-            DBMS_OUTPUT.PUT_LINE('Balance: ' || v_customer.AVAIL_BALANCE || '   -   ' || v_segment);
-            DBMS_OUTPUT.PUT_LINE(' ');
-        END LOOP;
-    CLOSE c_cursor;
-END;
-
---OTHER METHOD--
-
-
---4--
-DECLARE
-    CURSOR c_infor IS
-        SELECT
-            A.CUST_ID,
-            A.PRODUCT_CD,
-            P.NAME
-        FROM ACCOUNT A
-        JOIN PRODUCT P
-            ON A.PRODUCT_CD = P.PRODUCT_CD;
-BEGIN
-    FOR c IN c_infor LOOP
-        DBMS_OUTPUT.PUT_LINE('Customer ID: ' || c.CUST_ID || '   - Product CD: ' || c.PRODUCT_CD || '   - Product Name: ' || c.NAME);
+    OPEN C_PRODUCTS;
+    LOOP
+        FETCH C_PRODUCTS INTO V_PRODUCTS;
+        EXIT WHEN C_PRODUCTS%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE('Product CD: '
+                             || V_PRODUCTS.PRODUCT_CD);
+        DBMS_OUTPUT.PUT_LINE('Name         : '
+                             || V_PRODUCTS.NAME);
+        DBMS_OUTPUT.PUT_LINE(' ');
     END LOOP;
+
+    CLOSE C_PRODUCTS;
 END;
-
-
---5--
-CREATE TABLE ETL_CUSTOMER_T1 (
-    CUST_ID NUMBER,
-    SEGMENT VARCHAR2(20) NOT NULL,
-    ETL_DATE DATE NOT NULL
-);
-
--- SELECT * FROM ETL_CUSTOMER_T1;
-
+ --2--
 DECLARE
-    CURSOR c_info IS
-        SELECT 
-            A.CUST_ID,
-            I.FIRST_NAME,
-            I.LAST_NAME,
-            SUM(AVAIL_BALANCE) AS AVLB_BALANCE          
-        FROM ACCOUNT A
-        JOIN CUSTOMER C ON A.CUST_ID = C.CUST_ID
-        JOIN INDIVIDUAL I ON I.CUST_ID = C.CUST_ID
-        WHERE C.STATE = 'MA'
-        GROUP BY I.FIRST_NAME, I.LAST_NAME, A.CUST_ID;
-
-    v_level VARCHAR2(20);
-    v_count NUMBER;
-
+    CURSOR C_INFOR IS
+    SELECT
+        A.CUST_ID,
+        A.PRODUCT_CD,
+        P.NAME
+    FROM
+        ACCOUNT A
+        JOIN PRODUCT P
+        ON A.PRODUCT_CD = P.PRODUCT_CD;
+    V_INFOR C_INFOR%ROWTYPE;
 BEGIN
-    FOR c IN c_info LOOP
-        IF c.AVLB_BALANCE <= 4000 THEN
-            v_level := 'Low';
-        ELSIF c.AVLB_BALANCE > 4000 AND c.AVLB_BALANCE <= 7000 THEN
-            v_level := 'Medium';
-        ELSE
-            v_level := 'High';
+    OPEN C_INFOR;
+    LOOP
+        FETCH C_INFOR INTO V_INFOR;
+        EXIT WHEN C_INFOR%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE('Customer ID: '
+                             || V_INFOR.CUST_ID
+                             || '   - Product CD: '
+                             || V_INFOR.PRODUCT_CD
+                             || '   - Product Name: '
+                             || V_INFOR.NAME);
+    END LOOP;
+
+    CLOSE C_INFOR;
+END;
+ --3--
+DECLARE
+    CURSOR C_CURSOR IS
+    SELECT
+        I.FIRST_NAME
+        || ' '
+        || I.LAST_NAME       AS NAME,
+        SUM(A.AVAIL_BALANCE) AS AVAIL_BALANCE
+    FROM
+        CUSTOMER   C
+        JOIN ACCOUNT A
+        ON C.CUST_ID = A.CUST_ID
+        JOIN INDIVIDUAL I
+        ON I.CUST_ID = C.CUST_ID
+    GROUP BY
+        I.FIRST_NAME
+        || ' '
+        || I.LAST_NAME;
+    V_CUSTOMER C_CURSOR%ROWTYPE;
+    V_SEGMENT  VARCHAR2(20);
+BEGIN
+    OPEN C_CURSOR;
+    LOOP
+        FETCH C_CURSOR INTO V_CUSTOMER;
+        IF V_CUSTOMER.AVAIL_BALANCE <= 4000 THEN
+            V_SEGMENT := 'Low';
+        ELSIF V_CUSTOMER.AVAIL_BALANCE > 4000 AND V_CUSTOMER.AVAIL_BALANCE <= 7000 THEN
+            V_SEGMENT := 'Medium';
+        ELSIF V_CUSTOMER.AVAIL_BALANCE > 7000 THEN
+            V_SEGMENT := 'High';
         END IF;
 
-        INSERT INTO ETL_CUSTOMER_T1 VALUES(c.CUST_ID, v_level, SYSDATE);
+        EXIT WHEN C_CURSOR%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE('Customer: '
+                             || V_CUSTOMER.NAME);
+        DBMS_OUTPUT.PUT_LINE('Balance: '
+                             || V_CUSTOMER.AVAIL_BALANCE
+                             || '   -   '
+                             || V_SEGMENT);
+        DBMS_OUTPUT.PUT_LINE(' ');
     END LOOP;
-        COMMIT;
-        SELECT COUNT(*) INTO v_count
-        FROM ETL_CUSTOMER_T1
-        WHERE ETL_DATE = TRUNC(SYSDATE);
 
-    DBMS_OUTPUT.PUT_LINE('Total rows insert: ' || v_count);
-    DBMS_OUTPUT.PUT_LINE('Finished: ' || SYSTIMESTAMP);
+    CLOSE C_CURSOR;
+END;
+ --OTHER METHOD--
+ --4--
+DECLARE
+    CURSOR C_INFOR IS
+    SELECT
+        A.CUST_ID,
+        A.PRODUCT_CD,
+        P.NAME
+    FROM
+        ACCOUNT A
+        JOIN PRODUCT P
+        ON A.PRODUCT_CD = P.PRODUCT_CD;
+BEGIN
+    FOR C IN C_INFOR LOOP
+        DBMS_OUTPUT.PUT_LINE('Customer ID: '
+                             || C.CUST_ID
+                             || '   - Product CD: '
+                             || C.PRODUCT_CD
+                             || '   - Product Name: '
+                             || C.NAME);
+    END LOOP;
+END;
+ --5--
+CREATE TABLE ETL_CUSTOMER_T1 ( CUST_ID NUMBER, SEGMENT VARCHAR2(20) NOT NULL, ETL_DATE DATE NOT NULL );
+ -- SELECT * FROM ETL_CUSTOMER_T1;
+DECLARE
+    CURSOR C_INFO IS
+    SELECT
+        A.CUST_ID,
+        I.FIRST_NAME,
+        I.LAST_NAME,
+        SUM(AVAIL_BALANCE) AS AVLB_BALANCE
+    FROM
+        ACCOUNT    A
+        JOIN CUSTOMER C
+        ON A.CUST_ID = C.CUST_ID
+        JOIN INDIVIDUAL I
+        ON I.CUST_ID = C.CUST_ID
+    WHERE
+        C.STATE = 'MA'
+    GROUP BY
+        I.FIRST_NAME,
+        I.LAST_NAME,
+        A.CUST_ID;
+    V_LEVEL VARCHAR2(20);
+    V_COUNT NUMBER;
+BEGIN
+    FOR C IN C_INFO LOOP
+        IF C.AVLB_BALANCE <= 4000 THEN
+            V_LEVEL := 'Low';
+        ELSIF C.AVLB_BALANCE > 4000 AND C.AVLB_BALANCE <= 7000 THEN
+            V_LEVEL := 'Medium';
+        ELSE
+            V_LEVEL := 'High';
+        END IF;
+
+        INSERT INTO ETL_CUSTOMER_T1 VALUES(
+            C.CUST_ID,
+            V_LEVEL,
+            SYSDATE
+        );
+    END LOOP;
+
+    COMMIT;
+    SELECT
+        COUNT(*) INTO V_COUNT
+    FROM
+        ETL_CUSTOMER_T1
+    WHERE
+        ETL_DATE = TRUNC(SYSDATE);
+    DBMS_OUTPUT.PUT_LINE('Total rows insert: '
+                         || V_COUNT);
+    DBMS_OUTPUT.PUT_LINE('Finished: '
+                         || SYSTIMESTAMP);
 END;
--- course2/lec2/hw2.sql
+++ course2/lec2/hw2.sql
@@ -1,114 +1,153 @@
-
---1--  
+--1--
 BEGIN
-    FOR v_employee IN (
-        SELECT 
+    FOR V_EMPLOYEE IN (
+        SELECT
             E.EMP_ID,
             E.FIRST_NAME,
             E.LAST_NAME,
             D.DEPT_ID,
             D.NAME
-        FROM EMPLOYEE E
-        JOIN DEPARTMENT D
+        FROM
+            EMPLOYEE E
+            JOIN DEPARTMENT D
             ON E.DEPT_ID = D.DEPT_ID
-        WHERE E.ASSIGNED_BRANCH_ID = 1
-        ORDER BY E.EMP_ID ASC) LOOP
-        
-        DBMS_OUTPUT.PUT_LINE('ID: ' || v_employee.EMP_ID ||  '  -  ' || v_employee.FIRST_NAME || ' ' || v_employee.LAST_NAME);
-        DBMS_OUTPUT.PUT_LINE('Department ID: ' || v_employee.DEPT_ID || '   -  Department: ' || v_employee.NAME);
+        WHERE
+            E.ASSIGNED_BRANCH_ID = 1
+        ORDER BY
+            E.EMP_ID ASC
+    ) LOOP
+        DBMS_OUTPUT.PUT_LINE('ID: '
+                             || V_EMPLOYEE.EMP_ID
+                             || '  -  '
+                             || V_EMPLOYEE.FIRST_NAME
+                             || ' '
+                             || V_EMPLOYEE.LAST_NAME);
+        DBMS_OUTPUT.PUT_LINE('Department ID: '
+                             || V_EMPLOYEE.DEPT_ID
+                             || '   -  Department: '
+                             || V_EMPLOYEE.NAME);
         DBMS_OUTPUT.PUT_LINE(' ');
     END LOOP;
 END;
-
-
-
---2--
+ --2--
 DECLARE
-    v_level VARCHAR2(20);
-    v_count_open_acc NUMBER;
-    v_open_employee_id NUMBER := 10;
+    V_LEVEL            VARCHAR2(20);
+    V_COUNT_OPEN_ACC   NUMBER;
+    V_OPEN_EMPLOYEE_ID NUMBER := 10;
 BEGIN
-    SELECT COUNT(ACCOUNT_ID)
-    INTO v_count_open_acc
-    FROM ACCOUNT
-    WHERE OPEN_EMP_ID = v_open_employee_id;
-
-    IF v_count_open_acc <= 1 THEN
-        v_level := 'LOW';
-    ELSIF v_count_open_acc > 2 AND v_count_open_acc <= 4 THEN
-        v_level := 'AVG';
-    ELSIF v_count_open_acc > 4 AND v_count_open_acc <= 6 THEN
-        v_level := 'MODERATE';
+    SELECT
+        COUNT(ACCOUNT_ID) INTO V_COUNT_OPEN_ACC
+    FROM
+        ACCOUNT
+    WHERE
+        OPEN_EMP_ID = V_OPEN_EMPLOYEE_ID;
+    IF V_COUNT_OPEN_ACC <= 1 THEN
+        V_LEVEL := 'LOW';
+    ELSIF V_COUNT_OPEN_ACC > 2 AND V_COUNT_OPEN_ACC <= 4 THEN
+        V_LEVEL := 'AVG';
+    ELSIF V_COUNT_OPEN_ACC > 4 AND V_COUNT_OPEN_ACC <= 6 THEN
+        V_LEVEL := 'MODERATE';
     ELSE
-        v_level := 'HIGH';
+        V_LEVEL := 'HIGH';
     END IF;
 
-    DBMS_OUTPUT.PUT_LINE('Employee ID: ' || v_open_employee_id);
-    DBMS_OUTPUT.PUT_LINE('Number of account opened: ' || v_count_open_acc);
-    DBMS_OUTPUT.PUT_LINE('Level: ' || v_level);
+    DBMS_OUTPUT.PUT_LINE('Employee ID: '
+                         || V_OPEN_EMPLOYEE_ID);
+    DBMS_OUTPUT.PUT_LINE('Number of account opened: '
+                         || V_COUNT_OPEN_ACC);
+    DBMS_OUTPUT.PUT_LINE('Level: '
+                         || V_LEVEL);
 END;
-
-
-
---3--
+ --3--
 DECLARE
-    v_year NUMBER := 2000;
-    v_count NUMBER := 0;
-    V_total NUMBER := 0;
+    V_YEAR  NUMBER := 2000;
+    V_COUNT NUMBER := 0;
+    V_TOTAL NUMBER := 0;
 BEGIN
-    FOR v_year IN 2000 .. 2005 LOOP
-        SELECT COUNT(ACCOUNT_ID)
-        INTO v_count
-        FROM ACCOUNT
-        WHERE EXTRACT(YEAR FROM OPEN_DATE) = v_year;
-
-        DBMS_OUTPUT.PUT_LINE(' + Year: ' || v_year || '   Total accounts: ' || v_count);
-        
-        v_total := v_total + v_count;
+    FOR V_YEAR IN 2000 .. 2005 LOOP
+        SELECT
+            COUNT(ACCOUNT_ID) INTO V_COUNT
+        FROM
+            ACCOUNT
+        WHERE
+            EXTRACT(YEAR FROM OPEN_DATE) = V_YEAR;
+        DBMS_OUTPUT.PUT_LINE(' + Year: '
+                             || V_YEAR
+                             || '   Total accounts: '
+                             || V_COUNT);
+        V_TOTAL := V_TOTAL + V_COUNT;
     END LOOP;
 
-    DBMS_OUTPUT.PUT_LINE('Total accounts opened from 2000 -> 2005: ' || v_total);
+    DBMS_OUTPUT.PUT_LINE('Total accounts opened from 2000 -> 2005: '
+                         || V_TOTAL);
 END;
-
-
---4--
+ --4--
 DECLARE
-    CURSOR c_employee IS
-        SELECT
-            EMP_ID,
-            FIRST_NAME,
-            LAST_NAME
-        FROM EMPLOYEE;
-
-    v_first_date DATE;
+    CURSOR C_EMPLOYEE IS
+    SELECT
+        EMP_ID,
+        FIRST_NAME,
+        LAST_NAME
+    FROM
+        EMPLOYEE;
+    V_FIRST_DATE DATE;
 BEGIN
-    FOR c IN c_employee LOOP
-        SELECT MIN(OPEN_DATE)
-        INTO v_first_date
-        FROM ACCOUNT
-        WHERE OPEN_EMP_ID = c.EMP_ID;
-
-        IF v_first_date IS NULL THEN
-            v_first_date := SYSDATE;
+    FOR C IN C_EMPLOYEE LOOP
+        SELECT
+            MIN(OPEN_DATE) INTO V_FIRST_DATE
+        FROM
+            ACCOUNT
+        WHERE
+            OPEN_EMP_ID = C.EMP_ID;
+        IF V_FIRST_DATE IS NULL THEN
+            V_FIRST_DATE := SYSDATE;
         END IF;
 
-        DBMS_OUTPUT.PUT_LINE('Employee ID: ' || c.EMP_ID || '   ' || c.FIRST_NAME || ' ' || c.LAST_NAME);
-        DBMS_OUTPUT.PUT_LINE('First Date: ' || TO_CHAR(v_first_date, 'DD/MM/YYYY'));
+        DBMS_OUTPUT.PUT_LINE('Employee ID: '
+                             || C.EMP_ID
+                             || '   '
+                             || C.FIRST_NAME
+                             || ' '
+                             || C.LAST_NAME);
+        DBMS_OUTPUT.PUT_LINE('First Date: '
+                             || TO_CHAR(V_FIRST_DATE, 'DD/MM/YYYY'));
         DBMS_OUTPUT.PUT_LINE(' ');
     END LOOP;
 
     DBMS_OUTPUT.PUT_LINE(' ');
 END;
-
-
---5--
-SELECT * FROM EMPLOYEE
-
+ --5--
 DECLARE
-    CURSOR c_employee IS
-        SELECT * from EMPLOYEE;
-
-        
+    CURSOR C_EMPLOYEES IS
+    SELECT
+        EMP_ID,
+        FIRST_NAME,
+        LAST_NAME,
+        START_DATE
+    FROM
+        EMPLOYEE;
+    V_EXP_YEARS NUMBER;
+    V_SALARY    NUMBER;
 BEGIN
+    FOR C IN C_EMPLOYEES LOOP
+        V_EXP_YEARS := ROUND(MONTHS_BETWEEN(SYSDATE, C.START_DATE) / 12);
+        IF V_EXP_YEARS > 13 THEN
+            V_SALARY := 8000;
+        ELSIF V_EXP_YEARS > 11 AND V_EXP_YEARS <= 13 THEN
+            V_SALARY := 5000;
+        ELSIF V_EXP_YEARS > 9 AND V_EXP_YEARS <= 11 THEN
+            V_SALARY := 3000;
+        ELSIF V_EXP_YEARS > 7 AND V_EXP_YEARS <= 9 THEN
+            V_SALARY := 2000;
+        ELSIF V_EXP_YEARS > 4 AND V_EXP_YEARS <= 7 THEN
+            V_SALARY := 1000;
+        END IF;
 
-END
+        DBMS_OUTPUT.PUT_LINE(RPAD(C.EMP_ID, 15)
+                             || RPAD(C.FIRST_NAME
+                             || ' '
+                             || C.LAST_NAME, 30)
+                        || RPAD(C.START_DATE, 15)
+                           || V_SALARY);
+    END LOOP;
+END;
